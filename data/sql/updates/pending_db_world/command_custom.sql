861f05c7ba81cb9efc0dbd097c1165e4663aba42
dataCenter430 <titan032000@gmail.com>
2026-03-17 16:13:46 +0100

    fix: SQL code style

--- data/sql/updates/pending_db_world/command_custom.sql
+++ data/sql/updates/pending_db_world/command_custom.sql
@@ -2,8 +2,8 @@
 -- Applied after `command`. Duplicates: `command` wins unless Command.CustomOverridesCore = 1.
 
 CREATE TABLE IF NOT EXISTS `command_custom` (
-    `name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
+    `name` varchar(50) CHARACTER SET `utf8mb4` COLLATE `utf8mb4_unicode_ci` NOT NULL DEFAULT '',
     `security` tinyint unsigned NOT NULL DEFAULT '0',
-    `help` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
+    `help` longtext CHARACTER SET `utf8mb4` COLLATE `utf8mb4_unicode_ci`,
     PRIMARY KEY (`name`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='Custom/Module command overrides';
+) ENGINE=InnoDB DEFAULT CHARSET=`utf8mb4` COLLATE=`utf8mb4_unicode_ci` COMMENT='Custom/Module command overrides';
